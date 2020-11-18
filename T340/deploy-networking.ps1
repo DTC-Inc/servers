fccac5c8cda68a1dc78198d3c5ced0463f8759bd
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-11-18 01:56:20 -0500

    Update deploy-networking.ps1

--- T340/deploy-networking.ps1
+++ T340/deploy-networking.ps1
@@ -1,6 +1,14 @@
-Get-NetlbfoTeam | Remove-NetlbfoTeam -Confirm:$false
-$toTeam = Get-NetAdapter | Where-Object -Property InterfaceDescription -like "Broadcom NetXtreme Gigabit Ethernet*" | Select-Object -ExpandProperty Name
-New-NetlbfoTeam -Name TEAM0 -TeamMembers $toTeam -LoadBalancingAlgorithm Dynamic -TeamingMode SwitchIndependent -Confirm:$false
+#Remove all existing vNICs and vSwitches
+Get-VMNetworkadapter -managementOS  | Where-Object -property "name" -notlike "Container NIC*" | Remove-VMNetworkAdapter
+Get-VMSwitch | Where-Object -property name -notlike "Default Switch" | Remove-VMSwitch -force
+
+#Rename host to HV0 or HV1 etc.. Please check Automate if the name is available in the client
+$newName = Read-Host -prompt "Input the DTCBSURE Appliance Name (HV0, HV1, etc...): "
+Rename-Computer -newName $newName
+
+#Create SET team SET1 and Management vNIC. Also sets load balancing algorithm to dynamic
+$toTeam = Get-NetAdapter | Where-Object -property interfaceDescription -like "Broadcom*" | Select-Object -expandProperty name
+New-VMSwitch -name SET1 -netAdapterName $toTeam -enableEmbeddedTeaming $true
+Rename-VMNetworkAdapter -name SET1 -newName vNIC1 -managementOS
+Set-VMSwitchTeam -name SET1 -loadBalancingAlgorithm dynamic 
 ping 8.8.8.8 -n 30
-$toDisableIPv6 = Get-NetAdapter | Where-Object -Property Name -like "TEAM*" | Select-Object -ExpandProperty Name
-Disable-NetAdapterBinding -Name $toDisableIPv6 -ComponentID ms_tcpip6
