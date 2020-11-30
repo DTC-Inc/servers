3eec0b72e800a15e2ebd28b75a5345db4d018ac6
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-11-30 12:01:58 -0500

    Update deploy-networking.ps1

--- T340/deploy-networking.ps1
+++ T340/deploy-networking.ps1
@@ -1,12 +1,12 @@
-#Remove all existing vNICs and vSwitches
+# Remove all existing vNICs and vSwitches
 Get-VMNetworkadapter -managementOS  | Where-Object -property "name" -notlike "Container NIC*" | Remove-VMNetworkAdapter
 Get-VMSwitch | Where-Object -property name -notlike "Default Switch" | Remove-VMSwitch -force
 
-#Rename host to HV0 or HV1 etc.. Please check Automate if the name is available in the client
+# Rename host to HV0 or HV1 etc.. Please check Automate if the name is available in the client
 $newName = Read-Host -prompt "Input the server name (HV0, HV1, etc...)"
 Rename-Computer -newName $newName
 
-#Create SET team SET1 and Management vNIC. Also sets load balancing algorithm to dynamic
+# Create SET team SET1 and Management vNIC. Also sets load balancing algorithm to dynamic
 $toTeam = Get-NetAdapter | Where-Object -property interfaceDescription -like "Broadcom*" | Select-Object -expandProperty name
 New-VMSwitch -name SET1 -netAdapterName $toTeam -enableEmbeddedTeaming $true
 Rename-VMNetworkAdapter -name SET1 -newName vNIC1 -managementOS
