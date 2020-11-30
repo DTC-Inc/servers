d94ea409ca0bd347956c1c77fb94bcc88da50d9a
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-11-30 12:33:52 -0500

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -5,17 +5,66 @@ $errorCatch = $true
 while ($errorCatch -eq $true ) {
 
     #Read input of user on what type of server we're configuring
-    $input = Read-Host -prompt "What type of server are we configuring? (T140, T340, T440)"
-    echo "You chose $input"
+    $inputServer = Read-Host -prompt "What type of server are we configuring? (T140, T340, T440)"
+    Write-Host "You chose $input."
 
-    if ( $input -eq "T140" -or $input -eq "T340" ){
+    if ( $inputServer -eq "T140" -or $input -eq "T340" ){
+        Write-Host "You selected $inputServer."
+        $errorCatch = $false
+        
+    }else {
+        Write-Host "Input not accepted. Try again."
+
+    }
+}
+
+# Init errorCatch variable
+$errorCatch = $true
+
+# Start automation scripts
+while ($errorCatch -eq $true ) {
+
+    #Read input of user on what type of server we're configuring
+    $input = Read-Host -prompt "Will this server be a Hyper-V host? (y or n)"
+    echo "You chose $input"
+    if ( $inputServer -eq "T140" ) {
+        Write-Host "T140's cannot have Hyper-V role installed."
+        $input = n
+    }
     
-        if ( $input -eq "T140" ){
-            & "$psScriptRoot\T140\deploy-networking.ps1"
+    if ( $input -ne y or $input -ne n ){
+    
+        if ( $input -eq "y" ){
+            & "$psScriptRoot\deploy-hyperv.ps1"
+            
+            if ( $inputServer -eq "T340" ) {
+                & "$psScriptRoot\T340\deploy-networking-hyperv.ps1"
+            }
+            
+            if ( $inputServer -eq "T440" ) {
+                & "$psScriptRoot\T440\deploy-networking-hyperv.ps1"
+                
+            }
+            
             $errorCatch = $false
         }
-        if ( $input -eq "T340" ){
-            & "$psScriptRoot\T340\deploy-networking.ps1"
+        
+        if ( $input -eq "n" ){
+            Write-Host "Not deploying Hyper-V."
+            
+            if ( $inputServer -eq "T340" ) {
+                & "$psScriptRoot\T340\deploy-networking.ps1"
+            }
+            
+            if ( $inputServer -eq "T440" ) {
+                & "$psScriptRoot\T440\deploy-networking.ps1"
+                
+            }
+            
+            if ( $inputServer -eq "T140 ) {
+                & "$psScriptRoot\T140\deploy-networking.ps1"
+            }
+            
             $errorCatch = $false
         }    
     }else {
