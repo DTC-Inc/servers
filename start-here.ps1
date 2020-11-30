e918774e972a0c7d4468c43f6c93299a925326ff
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-11-30 11:45:56 -0500

    Update start-here.ps1

--- start-here.ps1
+++ start-here.ps1
@@ -13,28 +13,6 @@ Remove-Item -path $env:windir\temp\servers.zip -force -confirm:$false
 wget "https://codeload.github.com/DTC-Inc/servers/zip/main" -outFile $env:windir\temp\servers.zip
 Expand-Archive -path "$env:windir\temp\servers.zip" -destinationPath "$env:systemdrive\dtc" -force
 
-#Init $errorCatch variable
-$errorCatch = $true
+& "$psScriptRoot\deploy.ps1"
 
-#Start automation scripts
-while ($errorCatch -eq $true ) {
 
-    #Read input of user on what type of server we're configuring
-    $input = Read-Host -prompt "What type of server are we configuring? (T140, T340, T440)"
-    echo "You chose $input"
-
-    if ( $input -eq "T140" -or $input -eq "T340" ){
-    
-        if ( $input -eq "T140" ){
-            & "$psScriptRoot\T140\deploy-networking.ps1"
-            $errorCatch = $false
-        }
-        if ( $input -eq "T340" ){
-            & "$psScriptRoot\T340\deploy-networking.ps1"
-            $errorCatch = $false
-        }    
-    }else {
-        echo "Input not accepted. Try again"
-
-    }
-}
