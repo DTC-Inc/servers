d42d5743f5369b16d2b986b9ce59918acbcfa89a
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-12-02 11:56:47 -0500

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 # Init errorCatch variable
 Write-Host "Warning!! This will cause data loss if this is run!"
-$killScript "Do you want to kill this script? (y)"
+$killScript = Read-Host "Do you want to kill this script? (y)"
 
 if ( $killScript -eq "y" ) {
     exit
