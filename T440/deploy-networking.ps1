ed18cd5ba778c504f8c511fa99c114bd010d4057
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-12-02 17:51:22 -0500

    Update deploy-networking.ps1

--- T440/deploy-networking.ps1
+++ T440/deploy-networking.ps1
@@ -1,5 +1,8 @@
+# Remove all existing teams
 Get-NetlbfoTeam | Remove-NetlbfoTeam -Confirm:$false
-$toTeam = Get-NetAdapter | Where-Object -Property InterfaceDescription -like "Broadcom NetXtreme Gigabit Ethernet*" | Select-Object -ExpandProperty Name
+
+# Create team & disable IPv6
+$toTeam = Get-NetAdapter | Where-Object -Property InterfaceDescription -like "Intel*" | Select-Object -ExpandProperty Name
 New-NetlbfoTeam -Name TEAM0 -TeamMembers $toTeam -LoadBalancingAlgorithm Dynamic -TeamingMode SwitchIndependent -Confirm:$false
 ping 8.8.8.8 -n 30
 $toDisableIPv6 = Get-NetAdapter | Where-Object -Property Name -like "TEAM*" | Select-Object -ExpandProperty Name
