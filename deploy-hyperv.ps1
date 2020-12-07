4288866f5b1fbc11eceabcd0ed5ce9b776bfb5b8
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-12-07 18:10:15 -0500

    Delete deploy-hyperv.ps1

--- deploy-hyperv.ps1
+++
@@ -1,2 +0,0 @@
-Install-WindowsFeature -name Hyper-V,RSAT-Feature-Tools-Bitlocker,RSAT-Feature-Tools-Bitlocker-RemoteAdminTool,RSAT-Feature-Tools-BitLocker-BdeAducExt,BitLocker -includemanagementTools
-New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System\" -Name "InactivityTimeoutSecs" -Value 0x00000384 -PropertyType "DWord"
