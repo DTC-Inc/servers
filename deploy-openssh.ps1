b8b104cbf034e2e29e6738a1f680decfaf0bbde6
Nate Smith <40123869+nettts@users.noreply.github.com>
2020-12-07 20:21:36 -0500

    Update deploy-openssh.ps1

--- deploy-openssh.ps1
+++ deploy-openssh.ps1
@@ -5,10 +5,13 @@ Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
 Start-Service sshd
+
 # OPTIONAL but recommended:
 Set-Service -Name sshd -StartupType 'Automatic'
+
 # Change profile from Any to Private & Domain
 Set-NetFirewallRule -Name OpenSSH-Server-In-TCP -Profile Private,Domain
+
 # Confirm the Firewall rule is configured. It should be created automatically by setup.
 Get-NetFirewallRule -Name *ssh*
 
