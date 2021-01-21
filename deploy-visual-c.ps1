246f8108f61facf2835904959e033eab3d55c659
Nate Smith <40123869+nettts@users.noreply.github.com>
2021-01-21 01:01:51 -0500

    Update deploy-visual-c.ps1

--- deploy-visual-c.ps1
+++ deploy-visual-c.ps1
@@ -1,5 +1,6 @@
 
 # Download
+$progressPreference = 'SilentlyContinue'
 wget https://s3.us-west-002.backblazeb2.com/public-dtc/repo/vendors/msft/visutal-c-runtimes/visual-c-runtimes-all-in-one-aug-2020.zip -outFile $env:windir\temp\visual-c-runtimes-all-in-one-aug-2020.zip
 
 # Extract
