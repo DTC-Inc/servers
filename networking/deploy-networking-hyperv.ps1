5998624aecd0cfa5a294380a20bee549e73542b3
Nate Smith <40123869+nettts@users.noreply.github.com>
2022-12-19 10:16:39 -0500

    Update deploy-networking-hyperv.ps1
    Changed how the NIC interfaces are teamed via dedicated slots & embedded NICs

--- networking/deploy-networking-hyperv.ps1
+++ networking/deploy-networking-hyperv.ps1
@@ -1,32 +1,22 @@
-Write-Host "If Net adapters are vendor mixed, member count is always split between vendor."
-Write-Host "Only two seperate Teams will be created. The total NIC count will always be split in half between teams."
-$memberCount = Read-Host "Enter NICs Per Team (2 most common and default)"
-
-if (!$memberCount) {
-  $memberCount = 2
-
-}
+Write-Host "Net adapters will be teamed fully between amount of NICs per embedded & Slot 1, Slot 2, Slot 3, & Slot 4."
+Write-Host "***WARNING*** This script supports a maxmium of 1 embedded NI (all interfaces) & 4 NIC card slots."
 
 Get-NetLbfoTeam | Remove-NetLbfoTeam -confirm:$false
 Get-VMNetworkAdapter -managementOS | Where-Object -Property "name" -NotLike "Container NIC*" | Remove-VMNetworkAdapter
 Get-VMSwitch | Where-Object -Property name -NotLike "Default Switch" | Remove-VMSwitch -Force
 
-$nicList = Get-NetAdapter | Where-Object { ($_.InterfaceDescription -like "Intel*") -or ($_.InterfaceDescription -like "*Broadcom*") } | Select-Object -ExpandProperty InterfaceDescription
+$nicList = Get-NetAdapter | Where-Object { ($_.Name -like "Embedded*") -or ($_.Name -like "Slot 1**") } | Select-Object -ExpandProperty Name
 $nicCount = $nicList | Measure-Object | Select-Object -ExpandProperty Count
 
-if ($nicList -like "Intel*" -and $nicList -like "Broadcom*") {
-  $firstNicList = Get-NetAdapter | Where-Object -Property interfaceDescription -Like "Broadcom*" | Select-Object -ExpandProperty Name
-  $secondNicList = Get-NetAdapter | Where-Object -Property interfaceDescription -Like "Intel*" | Select-Object -ExpandProperty Name
-
-
-} elseif ($nicList -like "Broadcom*" -and $nicList -notlike "Intel*") {
-  $firstNicList = Get-NetAdapter | Where-Object { $_.InterfaceDescription -like "Broadcom*" } | Select-Object -Last $memberCount | Select-Object -ExpandProperty Name
-  $secondNicList = Get-NetAdapter | Where-Object { $_.InterfaceDescription -like "Broadcom*" } | Select-Object -First $memberCount | Select-Object -ExpandProperty Name
+if ($nicList -like "Embedded*" -and $nicList -like "Slot 1*") {
+  $firstNicList = Get-NetAdapter | Where-Object -Property Name -Like "Embedded*" | Select-Object -ExpandProperty Name
+  $secondNicList = Get-NetAdapter | Where-Object -Property Name -Like "Slot 1*" | Select-Object -ExpandProperty Name
 
 
 } else {
-  $firstNicList = Get-NetAdapter | Where-Object { $_.InterfaceDescription -like "Intel*" } | Select-Object -Last $memberCount | Select-Object -ExpandProperty Name
-  $secondNicList = Get-NetAdapter | Where-Object { $_.InterfaceDescription -like "Intel*" } | Select-Object -First $memberCount | Select-Object -ExpandProperty Name
+  $firstNicList = Get-NetAdapter | Where-Object { $_.Name -like "Embedded*" } | Select-Object -Last $memberCount | Select-Object -ExpandProperty Name
+  $secondNicList = Get-NetAdapter | Where-Object { $_.Name -like "Embedded*" } | Select-Object -First $memberCount | Select-Object -ExpandProperty Name
+
 
 }
 
